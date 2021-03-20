45d9235980e5ef3ba81f432459e85a821f82065b
hy <chessg55@gmail.com>
2021-03-20 17:45:02 +0900

    use class vs use useState

--- src/index.js
+++ src/index.js
@@ -4,19 +4,51 @@ import ReactDOM from "react-dom";
 import "./styles.css";
 
 // # use hooks
-const App = () => {
-  const [item, setItem] = useState(1);
-  const incrementItem = () => setItem(item + 1);
-  const decrementItem = () => setItem(item - 1);
-  return (
-    <div className="App">
-      <h1>Hello {item}</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <button onClick={incrementItem}>Increment</button>
-      <button onClick={decrementItem}>Decrement</button>
-    </div>
-  );
-};
+// const App = () => {
+//   const [item, setItem] = useState(1);
+//   const incrementItem = () => setItem(item + 1);
+//   const decrementItem = () => setItem(item - 1);
+//   return (
+//     <div className="App">
+//       <h1>Hello {item}</h1>
+//       <h2>Start editing to see some magic happen!</h2>
+//       <button onClick={incrementItem}>Increment</button>
+//       <button onClick={decrementItem}>Decrement</button>
+//     </div>
+//   );
+// };
+
+// # use class
+class App extends React.Component {
+  state = {
+    item: 1
+  };
+  render() {
+    const { item } = this.state;
+    return (
+      <div className="App">
+        <h1>Hello {item}</h1>
+        <h2>Start editing to see some magic happen!</h2>
+        <button onClick={this.incrementItem}>Increment</button>
+        <button onClick={this.decrementItem}>Decrement</button>
+      </div>
+    );
+  }
+  incrementItem = () => {
+    this.setState((state) => {
+      return {
+        item: state.item + 1
+      };
+    });
+  };
+  decrementItem = () => {
+    this.setState((state) => {
+      return {
+        item: state.item - 1
+      };
+    });
+  };
+}
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
