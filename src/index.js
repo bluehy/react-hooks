5bc85b2e72029286f131648073bc50da9cc277bf
hayoung <chessg55@gmail.com>
2021-09-29 17:38:46 +0900

    use Hooks

--- src/index.js
+++ src/index.js
@@ -1,29 +1,41 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-class App extends React.Component {
-  // state
-  state={
-    count: 0
-  }
+// 
+// class App extends React.Component {
+//   // state
+//   state={
+//     count: 0
+//   }
 
-  // function
-  modify = (n) => {
-    this.setState({
-      count: n
-    })
-  }
+//   // function
+//   modify = (n) => {
+//     this.setState({
+//       count: n
+//     })
+//   }
 
-  // render
-  render(){
-    const {count} = this.state;
-    return( 
+//   // render
+//   render(){
+//     const {count} = this.state;
+//     return( 
+//     <>
+//     <div>{count}</div>
+//     <button onClick={()=> this.modify(count + 1)}>Increment</button>
+//     </>
+//     )
+//   }
+// }
+
+//++++++ use Hooks+++++++
+const App = () => {
+  const [count, setCount] = useState(0);
+  return (
     <>
-    <div>{count}</div>
-    <button onClick={()=> this.modify(count + 1)}>Increment</button>
+    {count}
+    <button onClick={()=> setCount(count +1)}>Increment</button>
     </>
-    )
-  }
+  )
 }
 
 const rootElement = document.getElementById("root");
