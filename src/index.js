c25e73dcdb1888a8db3cc9452cfbd4b0743aeaa7
hayoung <chessg55@gmail.com>
2021-09-29 17:28:52 +0900

    react basic: class component, state, function, render etc

--- src/index.js
+++ src/index.js
@@ -1,53 +1,29 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-import "./styles.css";
-
-// # use hooks
-// const App = () => {
-//   const [item, setItem] = useState(1);
-//   const incrementItem = () => setItem(item + 1);
-//   const decrementItem = () => setItem(item - 1);
-//   return (
-//     <div className="App">
-//       <h1>Hello {item}</h1>
-//       <h2>Start editing to see some magic happen!</h2>
-//       <button onClick={incrementItem}>Increment</button>
-//       <button onClick={decrementItem}>Decrement</button>
-//     </div>
-//   );
-// };
-
-// # use class
 class App extends React.Component {
-  state = {
-    item: 1
-  };
-  render() {
-    const { item } = this.state;
-    return (
-      <div className="App">
-        <h1>Hello {item}</h1>
-        <h2>Start editing to see some magic happen!</h2>
-        <button onClick={this.incrementItem}>Increment</button>
-        <button onClick={this.decrementItem}>Decrement</button>
-      </div>
-    );
+  // state
+  state={
+    count: 0
   }
-  incrementItem = () => {
-    this.setState((state) => {
-      return {
-        item: state.item + 1
-      };
-    });
-  };
-  decrementItem = () => {
-    this.setState((state) => {
-      return {
-        item: state.item - 1
-      };
-    });
-  };
+
+  // function
+  modify = (n) => {
+    this.setState({
+      count: n
+    })
+  }
+
+  // render
+  render(){
+    const {count} = this.state;
+    return( 
+    <>
+    <div>{count}</div>
+    <button onClick={()=> this.modify(count + 1)}>Increment</button>
+    </>
+    )
+  }
 }
 
 const rootElement = document.getElementById("root");
