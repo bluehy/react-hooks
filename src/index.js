0b3ab14d814fa70a0f4e8711f6243d127038c524
hayoung <chessg55@gmail.com>
2021-09-30 15:22:41 +0900

    useInput example

--- src/index.js
+++ src/index.js
@@ -29,26 +29,41 @@ import ReactDOM from "react-dom";
 
 //++++++ use Hooks+++++++
 const App = () => {
-  // Hooks
+  // Hooks_useState
   const [count, setCount] = useState(0);
   const [email, setEmail] = useState("");
-  
   // function
-  const DecrementItem = () => setCount(count - 1);
-  
+  // arrow function
+  const DecrementItem = () => setCount(count - 1); 
+
   const updateEmail = e => {
     const {target: {value}} = e;
     setEmail(value);
   }
+  
+  // useInput
+  const useInput = (initialValue) => {
+    const [value, setValue] = useState(initialValue);
+    const onChange = (event) => {
+      console.log(event.target)
+    }
+    return { value, onChange };
+  }
+
 
   // return
+  const name = useInput("Ms.")
   return (
     <>
     {count}
     <button onClick={()=> setCount(count + 1)}>Increment</button>
     <button onClick={DecrementItem}>Decrement</button>
+
+    <h2> useInput </h2>
+    {/* <input placeholder="Name" value={name.value} onChange={name.onChange}/> */}
+    <input placeholder="Name" {...name}/>
+    {/* spread 연산자  */}
     <input placeholder="Email" value={email} onChange={updateEmail} />
-  
     </>
   )
 }
