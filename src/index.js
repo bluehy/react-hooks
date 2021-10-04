a6c2159f2cc9c8b89b63903b7f8cf993240337ea
hayoung <chessg55@gmail.com>
2021-10-04 11:47:25 +0900

    useClick.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import styled from "styled-components";
 import useInput from "./useInput";
 import useTabs  from "./useTabs";
 import UseTitleApp from "./useTitle";
+import useClick from "./useClick";
 
 const Category = styled.h2`
   font-size: 32px;
@@ -102,23 +103,6 @@ const App = () => {
   setTimeout(()=> console.log(input.current.focus()), 5000)
   // input으로 focus된다.
 
-  // ++++++++++Hooks_useClick++++++++++++++
-  const useClick = (onClick) => {
-    const element = useRef();
-    useEffect(()=> {
-      if(element.current){
-        element.current.addEventListener("click", onClick);
-      }
-      // -> mount (deps가 없는 한 이벤트리스너가 계속 살아있게 됨. 이후 같은 동작이 이어지면 계속 추가된다.)
-      return () => {
-        if(element.current){
-          element.current.removeEventListner("click", onClick);
-        }
-      }
-      // -> unmount 되었을 때 이벤트리스너를 remove해주어야 이후 mount가 다시 시도되었을 떄 하나의 이벤트 리스너만 업데이트 된다.
-    }, []);
-    return element;
-  }
   const sayHi = () => console.log("Hi");
   const title = useClick(sayHi);
   
