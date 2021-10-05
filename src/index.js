e31141201a608f953eac92e1cf86acbbe74a0fab
hayoung <chessg55@gmail.com>
2021-10-05 14:36:26 +0900

    if 위치 수정, clientY를 이용해 기능 작동 조건 추가

--- src/index.js
+++ src/index.js
@@ -66,17 +66,23 @@ const usePreventLeave = () => {
 
 // ++++++++++++useBeforeLeave+++++++++++++
 const useBeforeLeave = (onBefore) => {
-  if(typeof onBefore !== "function") {
-    return;
-  }
-  const handle = () => {
+  const handle = (event) => {
     // console.log("leaving");
-    onBefore();
+    // console.log(event);
+    const { clientY } = event;
+    if(clientY <= 0 ) {
+      // 위로 벗어났을 때(clientY<=0)에만 function이 작동하게 된다.
+      onBefore();
+    }
   }
   useEffect(() => {
     document.addEventListener("mouseleave", handle);
     return () => document.removeEventListener("mouseleave",handle);
   },[]);
+
+  if(typeof onBefore !== "function") {
+    return;
+  }
 };
 
 
