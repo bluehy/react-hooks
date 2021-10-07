3e78a7fb008d3b8ecf7568705318857becb1dd51
hayoung <chessg55@gmail.com>
2021-10-07 19:34:47 +0900

    useFullscreen _ cross browsing

--- src/index.js
+++ src/index.js
@@ -58,19 +58,36 @@ const useScroll = () => {
 // +++++++++++++++++++++++useFullscreen++++++++++++++++++
 const useFullscreen = (callback) => {
   const element = useRef();
+  const runCb = isFull => {
+    if(callback && typeof callback === "function"){
+      callback(isFull);
+    }
+  }
   const triggerFull = () => {
     if(element.current) {
-      element.current.requestFullscreen();
-      if(callback && typeof callback === "function"){
-        callback(true);
+      if(element.current.requestFullscreen){
+        element.current.requestFullscreen();
+      }else if (element.current.mozRequestFullscreen){
+        element.current.mozRequestFullscreen();
+      }else if (element.current.webkitRequestFullscreen){
+        element.current.webkitRequestFullscreen();
+      }else if (element.current.msRequestFullscreen){
+        element.current.msRequestFullscreen();
       }
+      runCb(true);
     }
   };
   const exitF = () => {
-    document.exitFullscreen();
-    if(callback && typeof callback === "function"){
-      callback(false);
+    if(document.exitFullscreen) {
+      document.exitFullscreen();
+    }else if (document.mozCancelFullscreen){
+      document.mozCancelFullscreen();
+    }else if (document.webkitExitFullscreen){
+      document.webkitExitFullscreen();
+    }else if (document.msExitFullscreen){
+      document.msExitFullscreen();
     }
+    runCb(false);
   };
   return {element, triggerFull, exitF};
 };
